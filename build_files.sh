187f723dd8ddf7570f4306b35e93399beaca5cef
Quiroz <quirozxc@gmail.com>
2025-09-07 23:54:15 -0400

    conf. para vervel #2.2

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 # build_files.sh
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
